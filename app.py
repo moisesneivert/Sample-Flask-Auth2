4b41a153d3ec1057ca04598a372f62e331ea0a4c
moisesneivert <143449564+moisesneivert@users.noreply.github.com>
2025-11-09 20:39:37 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, request, jsonify
 from models.user import User
 from database import db
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+import bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1:3306/flask-crud'
 
 login_manager = LoginManager()
 db.init_app(app)
@@ -25,36 +26,35 @@ def login():
     password = data.get("password")
 
     if username and password:
-       #Login
-       user = User.query.filter_by(username=username).first()
+        # Login
+        user = User.query.filter_by(username=username).first()
 
-       if user and user.password == password:
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
             login_user(user)
             print(current_user.is_authenticated)
             return jsonify({"message": "Autenticação realizada com sucesso"})
 
     return jsonify({"message": "Credenciais inválidas"}), 400
 
-@app.route('/logout', methods=["GET"])
+@app.route('/logout', methods=['GET'])
 @login_required
 def logout():
     logout_user()
-    return jsonify({"message": "Logout realizado com Sucesso!"})
-
+    return jsonify({"message": "Logout realizado com sucesso!"})
 
 @app.route('/user', methods=["POST"])
-@login_required
 def create_user():
     data = request.json
     username = data.get("username")
     password = data.get("password")
 
     if username and password:
-        user = User(username=username, password=password)
+        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+        user = User(username=username, password=hashed_password, role='user')
         db.session.add(user)
         db.session.commit()
         return jsonify({"message": "Usuario cadastrado com sucesso"})
-    
+
     return jsonify({"message": "Dados invalidos"}), 400
 
 @app.route('/user/<int:id_user>', methods=["GET"])
@@ -64,7 +64,7 @@ def read_user(id_user):
 
     if user:
         return {"username": user.username}
-    
+
     return jsonify({"message": "Usuario não encontrado"}), 404
 
 @app.route('/user/<int:id_user>', methods=["PUT"])
@@ -72,6 +72,9 @@ def read_user(id_user):
 def update_user(id_user):
     data = request.json
     user = User.query.get(id_user)
+
+    if id_user != current_user.id and current_user.role == "user":
+        return jsonify({"message": "Operação não permitida"}), 403
 
     if user and data.get("password"):
         user.password = data.get("password")
@@ -85,6 +88,9 @@ def update_user(id_user):
 @login_required
 def delete_user(id_user):
     user = User.query.get(id_user)
+    
+    if current_user.role != 'admin':
+        return jsonify({"message": "Operação não permitida"}), 403
 
     if id_user == current_user.id:
         return jsonify({"message": "Deleção não permitida"}), 403
@@ -92,7 +98,6 @@ def delete_user(id_user):
     if user:
         db.session.delete(user)
         db.session.commit()
-
         return jsonify({"message": f"Usuário {id_user} deletado com sucesso"})
     
     return jsonify({"message": "Usuario não encontrado"}), 404
